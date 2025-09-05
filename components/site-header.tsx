90ba8598fa29c759dcdf0b8b28d9f77f6d61fc3a
Yasser-Jamal-Al-Deen <yasserjamalaldeen@gmail.com>
2025-09-05 18:13:38 +0300

    fix

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -14,7 +14,7 @@ export function SiteHeader() {
   const t = useTranslations("")
 
   return (
-    <header className="bg-white flex h-20 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+    <header className="bg-white sticky top-0 z-10 flex h-20 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex justify-between w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <h1 className="text-2xl font-medium text-primary">{user?.pharmacyName}</h1>
 

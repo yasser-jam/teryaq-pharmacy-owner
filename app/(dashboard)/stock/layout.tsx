60bf0e3d35b0976ea49f6d2c69e1615cd005803e
Yasser-Jamal-Al-Deen <yasserjamalaldeen@gmail.com>
2025-09-06 12:53:19 +0300

    stock edits

--- app/(dashboard)/stock/layout.tsx
+++ app/(dashboard)/stock/layout.tsx
@@ -1,21 +1,36 @@
 "use client";
 import BaseHeader from "@/components/base/base-header";
 import BaseNotFound from "@/components/base/base-not-found";
+import BaseSearch from "@/components/base/base-search";
 import BaseSkeleton from "@/components/base/base-skeleton";
 import { StockCard } from "@/components/stock/stock-card";
 import { StockMiniCard } from "@/components/stock/stock-mini-card";
 import SysInfo from "@/components/sys/sys-info";
+import { Select } from "@/components/ui/select";
 import { api } from "@/lib/api";
 import { StockItem } from "@/types";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const { data: stockItems, isFetching } = useQuery<StockItem[]>({
+  const { data: stockItems, isFetching, refetch } = useQuery<StockItem[]>({
     queryKey: ["stock-items"],
-    queryFn: () => api("/stock/products/Overall"),
+    queryFn: () => api("/stock/search", {
+      params: {
+        lang: 'en',
+        keyword: search
+      }
+    }),
   });
+
+  const [selectedLang, setSelectedLang] = useState<'en' | 'ar'>('en')
+  const [search, setSearch] = useState<string>('')
+
+  useEffect(() => {
+    refetch()
+  }, [selectedLang, search])
+
 
   return (
     <>
@@ -23,6 +38,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         title="Stock Management"
         subtitle="Manage and track your stock items"
       ></BaseHeader>
+
+      <div>
+        <BaseSearch value={search} onChange={setSearch} className="my-4 w-full" />
+        {/* <Select
+          value={selectedLang}
+          onValueChange={(value) => setSelectedLang(value as 'en' | 'ar')} ></Select> */}
+      </div>
 
       <SysInfo
         className="mt-4"
